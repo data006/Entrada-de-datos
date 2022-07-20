226fecd2f09a7f9fc10d82de97ab61021ff03c50
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 15:21:35 -0600

    Add files via upload

---
+++ 04-entrada de datos/entradas.py
@@ -0,0 +1,17 @@
+"""
+
+Entrada de datos en python
+
+C++:
+    scanf("tipo",&localidad)
+
+Python
+    variable = input()
+
+"""
+
+numero = input("Ingresa un numero entero positivo: ")
+print(type(numero))
+
+dato = bool(input("Ingresa un dato bool: "))
+print(type(dato))
